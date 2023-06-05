0ec3bac23cea24bc2746780037f67592bc5c1340
jiapril11 <tadadadacoding@gmail.com>
2023-06-05 15:51:40 +0900

    Style: 코드 포맷팅 초기화

--- modules/list.js
+++ modules/list.js
@@ -1,37 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() { // 스크립트가 최상단에 있기 때문에, html 모든 요소가 파싱되고, 로드된 후에 이벤트를 발생 시키기 위함.
-    searchMovies();
+document.addEventListener('DOMContentLoaded', function () {
+  // 스크립트가 최상단에 있기 때문에, html 모든 요소가 파싱되고, 로드된 후에 이벤트를 발생 시키기 위함.
+  searchMovies();
 
-    
-    function searchMovies() {
+  function searchMovies() {
+    // TMDB Api
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmM2ODZkMDdhMTcxNDUyZWUyYzM2MGUwODFjNjAxMyIsInN1YiI6IjY0NzRjMGFhNWNkMTZlMDBiZjEyNDQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgYpZS6S6GHYU8eq2rRevqoZz5g80tZ7hZ5KJ3soHVU'
+      }
+    };
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 
-        // TMDB Api
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmM2ODZkMDdhMTcxNDUyZWUyYzM2MGUwODFjNjAxMyIsInN1YiI6IjY0NzRjMGFhNWNkMTZlMDBiZjEyNDQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgYpZS6S6GHYU8eq2rRevqoZz5g80tZ7hZ5KJ3soHVU'
-            }
-        };
-        const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'
-        
-        // api 데이터 긇어오기
-        fetch(url, options)
-            .then(response => response.json())
-            .then(data => {
-                let rows = data['results'];
-                const cardList = document.querySelector('.cardList');
-                cardList.innerHTML = '';
-                
-                
-                console.log(data)
-                rows.forEach((a) => {
-                    let _title = a['title'];
-                    let _overview = a['overview'];
-                    let _poster_path = a['poster_path'];
-                    let _vote_average = a['vote_average'];
-                    let _id = a['id'];
+    // api 데이터 긇어오기
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        let rows = data['results'];
+        const cardList = document.querySelector('.cardList');
+        cardList.innerHTML = '';
 
-                    let temp_html = `
+        console.log(data);
+        rows.forEach(a => {
+          let _title = a['title'];
+          let _overview = a['overview'];
+          let _poster_path = a['poster_path'];
+          let _vote_average = a['vote_average'];
+          let _id = a['id'];
+
+          let temp_html = `
                         <div class="movie-card" data-id="${_id}">
                             <img src="https://image.tmdb.org/t/p/w500${_poster_path}">
                             <h3 class ="info-title">${_title}</h3>
@@ -39,48 +38,47 @@ document.addEventListener('DOMContentLoaded', function() { // 스크립트가 �
                             <p class ="info-vote_average">Rating: ${_vote_average}</p>
                         </div>
                     `;
-                    cardList.insertAdjacentHTML('beforeend', temp_html);
-                });
+          cardList.insertAdjacentHTML('beforeend', temp_html);
+        });
 
-                // 클릭 이벤트 핸들러 추가 -> alert id
-                const movieCards = document.querySelectorAll('.movie-card'); // :CSS 선택자를 이용하여 모든 요소를 선택합니다.
-                movieCards.forEach(card => {
-                    card.addEventListener('click', function() {
-                        let movieId = this.getAttribute('data-id') // : 해당 요소의 속성 값을 가져옵니다.
-                        alert(`영화 id: ${movieId}`);
-                    });
-                });
+        // 클릭 이벤트 핸들러 추가 -> alert id
+        const movieCards = document.querySelectorAll('.movie-card'); // :CSS 선택자를 이용하여 모든 요소를 선택합니다.
+        movieCards.forEach(card => {
+          card.addEventListener('click', function () {
+            let movieId = this.getAttribute('data-id'); // : 해당 요소의 속성 값을 가져옵니다.
+            alert(`영화 id: ${movieId}`);
+          });
+        });
 
-                $('.movie-card').click(function () {
-                    let detail_modal = $('.detail-overlay');
-    
-                    let id = $(this).data('num');
-    
-                    $('#overlay').attr('data-id', id);
-    
-                    let all_id = rows.map(rows => rows['_id']);
-    
-                    let indexNum = $.inArray(id, all_id);
-    
-                    let this_data = rows[indexNum]
-    
-                    let $title = this_data['title']
-                    let $overview = this_data['overview'];
-                    let $poster_path = this_data['poster_path'];
-                    let $vote_average = this_data['vote_average'];
-                    let _id = this_data['num'];
-    
-                    $('.movie-card img').attr('src', $poster_path);
-                    $('.info-title').text($title);
-                    $('.info-overview').text($overview);
-                    $('.info-vote_average').text($vote_average);
-    
-                    detail_modal.css({
-                        'display': 'block'
-                    });
-                });
-            })
-            .catch(err => console.error(err));
-    }
+        $('.movie-card').click(function () {
+          let detail_modal = $('.detail-overlay');
 
+          let id = $(this).data('num');
+
+          $('#overlay').attr('data-id', id);
+
+          let all_id = rows.map(rows => rows['_id']);
+
+          let indexNum = $.inArray(id, all_id);
+
+          let this_data = rows[indexNum];
+
+          let $title = this_data['title'];
+          let $overview = this_data['overview'];
+          let $poster_path = this_data['poster_path'];
+          let $vote_average = this_data['vote_average'];
+          let _id = this_data['num'];
+
+          $('.movie-card img').attr('src', $poster_path);
+          $('.info-title').text($title);
+          $('.info-overview').text($overview);
+          $('.info-vote_average').text($vote_average);
+
+          detail_modal.css({
+            display: 'block'
+          });
+        });
+      })
+      .catch(err => console.error(err));
+  }
 });
--- modules/search.js
+++ modules/search.js
@@ -1,83 +1,80 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const frm = document.search;
+  frm.addEventListener('submit', handleSearch);
 
-    const frm = document.search;
-    frm.addEventListener('submit', handleSearch)
+  function handleSearch(event) {
+    event.preventDefault(); // 폼 제출 기본 동작 방지
 
+    const searchInput = document.getElementById('searchInput');
+    const query = searchInput.value.toLowerCase();
 
-    function handleSearch(event) {
-        event.preventDefault(); // 폼 제출 기본 동작 방지
+    console.log(query);
 
-        const searchInput = document.getElementById('searchInput');
-        const query = searchInput.value.toLowerCase()
+    // 검색어가 비어있을 경우 처리
+    if (query.trim() === '') {
+      alert('검색어를 입력해주세요.');
+      return;
+    }
 
-        console.log(query)
+    // TMDB Api
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmM2ODZkMDdhMTcxNDUyZWUyYzM2MGUwODFjNjAxMyIsInN1YiI6IjY0NzRjMGFhNWNkMTZlMDBiZjEyNDQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgYpZS6S6GHYU8eq2rRevqoZz5g80tZ7hZ5KJ3soHVU'
+      }
+    };
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 
-        // 검색어가 비어있을 경우 처리
-        if (query.trim() === '') {
-            alert('검색어를 입력해주세요.');
-            return;
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        const searchResults = data['results'];
+        // 전체 데이터 들 제목을 소문자로 변환
+        let titleList = searchResults.map(item => {
+          return item.title.toLowerCase();
+        });
+        // query검색어와  위에 값이 포함 되어 있는 타이틀 반환
+        let find_title = titleList.filter(item => {
+          return item.includes(query);
+        });
+
+        // 전체 타이틀 리스트에서 일치하는 타이틀의 인덱스 번호 찾기
+        let find_index = [];
+
+        for (let i in find_title) {
+          let idx = titleList.findIndex(item => {
+            return item === find_title[i];
+          });
+
+          find_index.push(idx);
         }
 
+        // 값이 없으면 -> alert ,
+        if (find_index.length === 0) {
+          alert('검색 결과가 없습니다.');
+          // 값이 있으면 ->
+        } else {
+          const cardList = document.querySelector('.cardList');
+          // 전체 데이터에서 일치한 데이터 뽑아오기
 
-        // TMDB Api
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmM2ODZkMDdhMTcxNDUyZWUyYzM2MGUwODFjNjAxMyIsInN1YiI6IjY0NzRjMGFhNWNkMTZlMDBiZjEyNDQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgYpZS6S6GHYU8eq2rRevqoZz5g80tZ7hZ5KJ3soHVU'
-            }
-        };
-        const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+          const match_movie = [];
+          for (let a of find_index) {
+            const movies = searchResults[a];
+            match_movie.push(movies);
+          }
 
-        fetch(url, options)
-            .then(response => response.json())
-            .then(data => {
-                const searchResults = data['results'];
-                // 전체 데이터 들 제목을 소문자로 변환
-                let titleList = searchResults.map((item) => {
-                    return item.title.toLowerCase()
-                })
-                // query검색어와  위에 값이 포함 되어 있는 타이틀 반환
-                let find_title = titleList.filter((item) =>{
-                    return item.includes(query)
-                }) ;
-                
-                // 전체 타이틀 리스트에서 일치하는 타이틀의 인덱스 번호 찾기
-                let find_index = []
+          cardList.innerHTML = '';
+          // 채워넣기
+          match_movie.forEach(result => {
+            const title = result['title'];
+            const overview = result['overview'];
+            const posterPath = result['poster_path'];
+            const voteAverage = result['vote_average'];
+            const id = result['id'];
 
-                for (let i in find_title){
-                    let idx = titleList.findIndex((item) => {
-
-                        return item === find_title[i]
-                    });
-
-                    find_index.push(idx);
-                }
-
-                // 값이 없으면 -> alert , 
-                if (find_index.length === 0) {
-                    alert('검색 결과가 없습니다.');
-                // 값이 있으면 ->     
-                } else {
-                    const cardList = document.querySelector('.cardList');
-                    // 전체 데이터에서 일치한 데이터 뽑아오기 
-
-                    const match_movie = []
-                    for (let a of find_index) {
-                        const movies = searchResults[a];
-                        match_movie.push(movies);
-                    }
-                
-                    cardList.innerHTML = '';
-                    // 채워넣기
-                    match_movie.forEach((result) => {
-                        const title = result['title'];
-                        const overview = result['overview'];
-                        const posterPath = result['poster_path'];
-                        const voteAverage = result['vote_average'];
-                        const id = result['id'];
-
-                        const temp_html = `
+            const temp_html = `
                     <div class="movie-card" data-id="${id}">
                         <img src="https://image.tmdb.org/t/p/w500${posterPath}">
                         <h3>${title}</h3>
@@ -85,10 +82,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         <p>Rating: ${voteAverage}</p>
                     </div>
                 `;
-                        cardList.insertAdjacentHTML('beforeend', temp_html);
-                    });
-                }
-            })
-            .catch(err => console.error(err));
-    };
+            cardList.insertAdjacentHTML('beforeend', temp_html);
+          });
+        }
+      })
+      .catch(err => console.error(err));
+  }
 });
